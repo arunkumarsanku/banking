3239444901a3d15a9b715d1f912d487c6d3343f1
arunkumarsanku <arunkumarsanku0408@gmail.com>
2025-01-29 23:57:21 +0100

    implementing sidebar for mobilenav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,13 +8,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  const loggedIn = {firstName:'Arun', lastName:'Kumar'};
+  const loggedIn = { firstName: 'Arun', lastName: 'Kumar' };
   return (
     <main className="flex h-screen w-full font-inter">
-        
-        <Sidebar user = { loggedIn} />
 
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={24} height={24} alt="logo" />
+          <div className="">
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
         {children}
+      </div>
+
+      
     </main>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -16,7 +16,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
             <nav className="flex flex-col gap-4">
                 <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
                     <Image src="/icons/logo.svg" width={34} height={34} alt="Horizon logo" className="size-[24px] max-xl:size-14" />
-                    <h1 className="sidebar-logo">Horizon</h1>
+                    <h1 className="sidebar-logo">Horizon</h1> 
                 </Link>
 
                 {sidebarLinks.map((item) => {
@@ -29,7 +29,11 @@ const Sidebar = ({ user }: SiderbarProps) => {
 
                     </Link>)
                 })}
+
+                USER
             </nav>
+
+            FOOTER
         </section>
     )
 }
