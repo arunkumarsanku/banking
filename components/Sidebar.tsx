bb0ed027157370a6dcb67c4f9b9e4cbe9255bd9c
arunkumarsanku <arunkumarsanku0408@gmail.com>
2025-02-01 01:33:40 +0100

    setting up the other routes in sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -16,7 +16,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
             <nav className="flex flex-col gap-4">
                 <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
                     <Image src="/icons/logo.svg" width={34} height={34} alt="Horizon logo" className="size-[24px] max-xl:size-14" />
-                    <h1 className="sidebar-logo">Horizon</h1> 
+                    <h1 className="sidebar-logo">Horizon</h1>
                 </Link>
 
                 {sidebarLinks.map((item) => {
@@ -25,7 +25,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
                         <div className="relative size-6">
                             <Image src={item.imgURL} alt={item.label} fill className={cn({ 'brightness-[3] invert-0': isActive })} />
                         </div>
-                        <p className={cn('sidebar-label',{'!text-white' :isActive})}>{item.label}</p>
+                        <p className={cn('sidebar-label', { '!text-white': isActive })}>{item.label}</p>
 
                     </Link>)
                 })}
--- types/index.d.ts
+++ types/index.d.ts
@@ -212,7 +212,7 @@ declare interface FooterProps {
   user: User;
 }
 
-declare interface RightSidebarProps {
+declare interface   RightSidebarProps {
   user: User;
   transactions: Transaction[];
   banks: Bank[] & Account[];
